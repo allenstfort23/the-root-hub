954725d026e4f681ca4103fa3c23cc65932e0134
allensftort <allenstfort1@gmail.com>
2025-02-08 01:02:04 -0500

    Update About-Us page

--- client/src/pages/About/AboutUs.tsx
+++ client/src/pages/About/AboutUs.tsx
@@ -1,5 +1,109 @@
 const AboutUs = () => {
-  return <h1>Us</h1>;
+  return (
+    <div className="contianer-fluid">
+      <h1 className="display-6 text-center py-4">About Us</h1>
+
+      <div className="row justify-content-around">
+        <div className="left-content">
+          <h3>Welcome to The Root Hub!</h3>
+          <p className="">
+            At The Root Hub, we believe in the power of community, innovation,
+            and representation. Our platform is dedicated to empowering Black
+            tech professionals by providing a space for sharing knowledge,
+            building connections, and celebrating success in the tech world.
+          </p>
+          <p>
+            We understand that the tech industry has often lacked diversity, and
+            we’re here to change that. Our mission is to foster an inclusive
+            environment where Black tech creators, entrepreneurs, and innovators
+            can thrive, collaborate, and elevate one another.
+          </p>
+        </div>
+        <div className="pl-2">
+          <h3>Our Mission</h3>
+          <p>
+            <h5>The Root Hub aims to:</h5>
+            <ul>
+              <li>
+                <b>Create a space for Black tech:</b> professionals to showcase
+                their skills and talents.
+              </li>
+              <li>
+                <b>Connect individuals:</b> to career opportunities, resources,
+                and mentorship.
+              </li>
+              <li>
+                <b>Provide a platform for learning:</b>through insightful posts,
+                tech discussions, and tutorials.
+              </li>
+              <li>
+                <b>Celebrate and uplift:</b> Black tech community by sharing
+                achievements, stories, and innovations.
+              </li>
+            </ul>
+          </p>
+        </div>
+        <div className="">
+          <h3>Why “The Root Hub”?</h3>
+          <p>
+            The name “Root Hub” symbolizes the foundation from which all things
+            grow. Just like a tree’s roots provide nourishment, The Root Hub
+            serves as a source of support for Black tech professionals to grow,
+            connect, and reach their full potential. We aim to be a central hub
+            for resources, discussions, and tools that help shape the future of
+            technology while promoting inclusivity, diversity, and cultural
+            pride.
+          </p>
+        </div>
+        <div className="pl-2">
+          <h3>What We Offer</h3>
+          <p>
+            <ul>
+              <li>
+                <b>Tech Content: </b> Stay updated with the latest trends,
+                tutorials, and insights from the Black tech community.
+              </li>
+              <li>
+                <b>Career Resources:</b> Find job listings, scholarships, and
+                mentorship programs tailored to Black tech professionals.
+              </li>
+              <li>
+                <b>Networking Opportunities:</b> Connect with like-minded
+                individuals, share your journey, and collaborate on projects.
+              </li>
+              <li>
+                <b>Inspiration:</b> Discover stories of success, perseverance,
+                and innovation from Black leaders in tech.
+              </li>
+            </ul>
+          </p>
+        </div>
+        <div className="">
+          <h4>Our Values</h4>
+          <p>
+            • Inclusivity: We value diversity and strive to ensure everyone,
+            regardless of background, feels welcome and empowered to contribute.
+            • Collaboration: We believe in the power of teamwork and shared
+            knowledge. Together, we are stronger. • Growth: Personal and
+            professional growth is at the core of what we do, whether it’s
+            through learning new skills or advancing careers. • Pride: We take
+            immense pride in celebrating Black culture, achievements, and
+            excellence in the tech world.
+          </p>
+        </div>
+        <div className="">
+          <h4>Join Us</h4>
+          <p>
+            At The Root Hub, everyone is welcome. Whether you’re just starting
+            your career or you’re a seasoned professional, we have a space for
+            you. We invite you to join us on this journey to make a lasting
+            impact on the world of tech. Together, we can make a difference.
+          </p>
+        </div>
+        <h2>Let’s build, innovate, and elevate — together.</h2>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
